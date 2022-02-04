830827baec5a6a1f53d74f0d62e9da9e0f23dd41
Spencer Johnson <sljohnson2222@gmail.com>
2022-02-03 20:06:06 -0600

    commit tree 3b712b04747f028e5c2c0d8466772646da07386f

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+// findMatching - takes in an array of driver' names and a STRING as arguments
+//returns matching list of drivers (has to be case sensetive).
+
+
+// function findMatching(driversName, name) {
+//     for (const drivers of driversName) {
+//         console.log(drivers);
+//     }
+// }
+
+// findMatching(drivers)
+
+function findMatching(driversName, name) {
+    for (const drivers of driversName) {
+        if (drivers === "Bobby") {
+            return [drivers]
+        }
+    }
+}
+
+findMatching(drivers, "Bobby")
